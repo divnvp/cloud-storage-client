6ef26f5dcc88e26f6c3c080c5c09116461faa360
Diana <divnvp@yandex.com>
2022-08-06 20:36:24 +0300

    add: UIAuth.vue component for authorization

---
+++ src/components/UIAuth.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-dialog v-model="show" persistent width="460px">
+    <v-card :loading="loading">
+      <v-card-title>
+        Авторизация
+      </v-card-title>
+
+      <v-card-text>
+        <v-form @submit.prevent="auth" v-model="valid">
+          <v-text-field
+            v-model="user.username"
+            color="primary"
+            :rules="formRules"
+            label="Имя пользователя"
+          />
+
+          <v-text-field
+            v-model="user.password"
+            type="password"
+            color="primary"
+            :rules="formRules"
+            label="Пароль"
+          />
+
+          <v-alert v-if="error && !loading" dense text type="error">
+            Неверное имя пользователя или пароль!
+          </v-alert>
+
+          <v-row fluid>
+            <v-btn text color="primary" type="submit" :disabled="!valid">
+              Войти в хранилище
+            </v-btn>
+
+            <v-spacer />
+
+            <v-btn text color="primary" type="submit" :disabled="!valid">
+              Регистрация
+            </v-btn>
+          </v-row>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script defer>
+export default {
+  name: "UIAuth",
+
+  data: () => ({
+    show: true,
+    loading: false,
+    valid: false,
+    error: false,
+
+    user: {
+      username: "",
+      password: ""
+    },
+
+    formRules: [v => !!v || "Это обязательное пое"],
+  }),
+
+  async mounted() {
+    // await logout(); TODO
+  },
+
+  methods: {
+    async auth() {
+      this.loading = true;
+      this.error = false;
+      if (!(this.user.username && this.password)) return;
+
+      try {
+        // TODO
+        this.show = false;
+      } catch (e) {
+        this.error = true;
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+}
+</script>
