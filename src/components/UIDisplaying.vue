1ee400b3d15d35783b922aebe2fcc13f076f75f5
Diana <divnvp@yandex.com>
2022-08-09 19:57:23 +0300

    add: settings for displaying

---
+++ src/components/UIDisplaying.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-center">
+    <v-select
+      v-model="displaying"
+      :items="items"
+      :item-value="i => i.value"
+      :item-text="i => i.title"
+      label="Отобразить как"
+      outlined
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UIDisplaying",
+
+  props: {
+    value: { type: String, required: true }
+  },
+
+  data: () => ({
+    items: [
+      { title: 'Табличный', value: "table" },
+      { title: 'Сетка', value: "grid" }
+    ],
+  }),
+
+  computed: {
+    displaying: {
+      get() {
+        return this.value;
+      },
+
+      set(newValue) {
+        this.$emit("input", newValue);
+      }
+    },
+  }
+}
+</script>
---
+++ src/components/UISettings.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-dialog v-model="show" max-width="480px">
+    <template #activator="{on, attr}">
+      <v-btn
+        v-bind="attr" v-on="on"
+        text
+        block
+        class="my-2"
+        style="font-size: 12px"
+      >
+        Настройки
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title>
+        Настройки
+      </v-card-title>
+
+      <v-card-text>
+        <UIDisplaying v-model="displaying" />
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn text color="error" @click="show = false">
+          Отменить
+        </v-btn>
+
+        <v-spacer />
+
+        <v-btn text color="primary" @click="saveSetting()">
+          Сохранить
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+// Components
+import UIDisplaying from "./UIDisplaying";
+
+export default {
+  name: "UISettings",
+
+  components: { UIDisplaying },
+
+  data:() => ({
+    show: false,
+
+    displaying: "table"
+  }),
+
+  methods: {
+    saveSetting() {
+      this.$emit("save", this.displaying);
+      this.show = false;
+    }
+  }
+}
+</script>
