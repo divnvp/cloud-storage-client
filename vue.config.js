7c2bf9decd464e19e7298a6e82887bd991bf2e0c
Diana <divnvp@yandex.com>
2022-08-11 10:17:55 +0300

    fix: app

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "cloud-storage-client" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/cloud-storage-client/" : "/"
 }
